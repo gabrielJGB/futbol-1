5a073de0dc71d179fba49ba1a851fca1f1b23328
Gabriel <brunengo.gabriel@gmail.com>
2026-03-01 23:49:44 -0300

    update

--- src/hooks/useHome.js
+++ src/hooks/useHome.js
@@ -2,41 +2,39 @@ import useSWR from 'swr';
 import { fetcher } from '@/utils/fetcher';
 const CORS_PROXY = "https://corsproxy.io/?"
 
-
-/*  --test data-- */
-import data from '@/data/dummy/TODAY.json'
 export const useHome = (date) => {
 
-    
+    const intervalEnabled = date === "hoy"
+    date = date === "hoy" ? "today" : date
+
+    const data = useSWR(
+        date ? `https://api.promiedos.com.ar/games/${date}` : null,
+        fetcher,
+        {
+            revalidateIfStale: true,
+            revalidateOnFocus: false,
+            refreshInterval: intervalEnabled ? 20000 : null
+        }
+    );
 
     return {
-        data,
-        isLoading: false,
-        error: false
-    }
+        data: data.data,
+        isLoading: data.isLoading,
+        error: data.error
+    };
 }
 
 
 
-
+/*  --test data-- */
+// import data from '@/data/dummy/TODAY.json'
 // export const useHome = (date) => {
 
-//     const intervalEnabled = date === "hoy"
-//     date = date === "hoy" ? "today" : date
-
-//     const data = useSWR(
-//         date ? `https://api.promiedos.com.ar/games/${date}` : null,
-//         fetcher,
-//         {
-//             revalidateIfStale: true,
-//             revalidateOnFocus: false,
-//             refreshInterval: intervalEnabled ? 20000 : null
-//         }
-//     );
+    
 
 //     return {
-//         data: data.data,
-//         isLoading: data.isLoading,
-//         error: data.error
-//     };
+//         data,
+//         isLoading: false,
+//         error: false
+//     }
 // }
